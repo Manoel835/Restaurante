a42ab2dc78dba196c2df4b9e05d3569c20ad3393
Manoel835 <mftneto@gmail.com>
2023-06-23 13:47:15 -0300

    Title

--- src/pages/Main/Drinks/index.tsx
+++ src/pages/Main/Drinks/index.tsx
@@ -1,3 +1,10 @@
+import { Head } from "../../../components/Head"
+import { SnackTitle } from "../../../components/SnackTitle"
 export default function Drinks(){
-  return <h1>Drinks dos melhores</h1>
+  return(
+    <>
+      <Head title="Bebidas"/>
+      <SnackTitle>Bebidas</SnackTitle>
+    </>
+    )
 }
