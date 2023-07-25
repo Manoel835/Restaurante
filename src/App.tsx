afbe4e693e084ea9fa4520ed1327ba36472fb27d
Manoel835 <mftneto@gmail.com>
2023-07-25 15:33:34 -0300

    Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
 import { BrowserRouter } from 'react-router-dom'
-
+import { createContext, useState, useEffect} from 'react'
 import { AppRoutes } from './routes'
-
 import { Theme } from './styles/Theme'
 import { GlobalStyle } from './styles/global'
 import { Normalize } from 'styled-normalize'
+import { SnackData } from './interfaces/SnackData'
+import { getBurgers } from './services/api'
+
+interface SnackContexProt{
+  burgers: SnackData[]
+}
+ export const SnackContext = createContext({} as SnackContexProt)
+
 
 export default function App() {
+  const [burgers, setBurgers] = useState<SnackData[]>([])
+
+  useEffect(() =>
+  {
+    ;(async () =>{
+      const burgerRequest = await getBurgers()
+      setBurgers(burgerRequest.data)
+    })()
+  },[])
   return (
     <BrowserRouter>
       <Theme>
+        <SnackContext.Provider value={{burgers}}>
         <AppRoutes />
         <GlobalStyle />
         <Normalize />
+        </SnackContext.Provider>
       </Theme>
     </BrowserRouter>
   )
