623cef0eb7dd79b7fea2e38a719d67577b60a861
Manoel835 <mftneto@gmail.com>
2023-08-08 12:52:57 -0300

    Navegação

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,4 +1,20 @@
 import { Container } from "./styles"
+import {ReactComponent as BurgerIcon} from "../../assets/burger.svg"
+import menuImg from '../../assets/menu.svg'
 export function Sidenar(){
-  return <Container>Sidebar</Container>
+  return <Container>
+    <button type="button">
+      <img src={menuImg}/>
+    </button>
+    <nav>
+      <ul>
+        <li>
+          <a href="#" className="active">
+            <BurgerIcon/>
+            <span>Hambúrgueres</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </Container>
 }
