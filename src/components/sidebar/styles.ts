0267ca6fdf1421c26151d4ab118aa02e6fcd600e
Manoel835 <mftneto@gmail.com>
2024-01-21 00:21:16 -0300

    Mobile

--- src/components/sidebar/styles.ts
+++ src/components/sidebar/styles.ts
@@ -1,38 +1,44 @@
-import styled, {css} from "styled-components";
+import styled, { css } from 'styled-components'
 
 interface ContainerProps {
-  isMenuOpen: boolean;
+  isMenuOpen: boolean
 }
 
 export const Container = styled.aside<ContainerProps>`
-background-color:${(props) => (props.theme.colors.secondary)};
-${({ isMenuOpen}) =>  isMenuOpen ? css` width:16.3rem;` : css`width:7.75rem;`
-}
-padding: 2rem 0;
-overflow: hidden;
-display: flex;
-flex-direction: column;
-align-items: center;
-transition: width 0.3s;
-  button{
+  background-color: ${(props) => props.theme.colors.secondary};
+  ${({ isMenuOpen }) =>
+    isMenuOpen
+      ? css`
+          width: 16.3rem;
+        `
+      : css`
+          width: 7.75rem;
+        `}
+  padding: 2rem 0;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  transition: width 0.3s;
+  button {
     background: none;
     width: 100%;
     border: none;
   }
-  nav{
+  nav {
     flex: 1;
     width: 100%;
     height: 100%;
 
-    ul{
+    ul {
       height: 100%;
       display: flex;
       flex-direction: column;
       justify-content: center;
       gap: 1.5rem;
     }
-    li{
-      a{
+    li {
+      a {
         width: fit-content;
         position: relative;
         padding-left: 1.875rem;
@@ -42,35 +48,79 @@ transition: width 0.3s;
         align-items: center;
         gap: 2rem;
 
-        svg{
-          fill: ${(props) => (props.theme.colors.red)};
+        svg {
+          fill: ${(props) => props.theme.colors.red};
           width: 4rem;
           height: 4rem;
           transition: fill 0.3s;
         }
-        span{
+        span {
           font-size: 1rem;
           font-weight: 500;
           transition: color 0.3s;
         }
-        &.active{
-          &::after{
+        &.active {
+          &::after {
             content: '';
             position: absolute;
             left: 0;
             top: 50%;
             bottom: 0;
             transform: translateY(-50%);
-            background-color: ${(props) => (props.theme.colors.red)};
+            background-color: ${(props) => props.theme.colors.red};
             width: 5px;
             height: calc(100% + 10px);
             border-radius: 0 5px 5px 0;
           }
-          svg{
-            fill: ${(props) => (props.theme.colors.red)};
+          svg {
+            fill: ${(props) => props.theme.colors.red};
           }
-          span{
-            color: ${(props) => (props.theme.colors.red)}
+          span {
+            color: ${(props) => props.theme.colors.red};
+          }
+        }
+      }
+    }
+  }
+  @media (max-width: 720px) {
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 999;
+    width: 100%;
+    height: 5rem;
+    overflow-y: auto;
+    padding: 0 0;
+    button {
+      display: none;
+    }
+    nav {
+      height: 100%;
+      ul {
+        flex-direction: row;
+        align-items: center;
+      }
+      li {
+        display: flex;
+        gap: 3.5rem;
+        a {
+          flex-direction: column;
+          padding: 0rem;
+
+          svg {
+            width: 3.25rem;
+            height: 3.25rem;
+          }
+
+          span {
+            display: none;
+          }
+
+          &.active {
+            &::after {
+              display: none;
+            }
           }
         }
       }
