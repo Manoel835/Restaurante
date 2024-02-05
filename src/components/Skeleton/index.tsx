6f3c284e5da95fe5fe49c8eea84faa87d08e1176
Manoel835 <mftneto@gmail.com>
2024-02-04 22:39:56 -0300

    Loading

---
+++ src/components/Skeleton/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { SkeletonElement } from './styles'
+export interface SkeletonProps {
+  type: 'title' | 'thumbnail' | 'text' | 'image'
+}
+
+export default function Skeleton({type}: SkeletonProps) {
+  return <SkeletonElement type={type} />
+}
